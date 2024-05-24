7f004b268fb59e929b728c9299f61cd3ee6250a8
lordsrule <128402434+lordsrule@users.noreply.github.com>
2024-05-24 19:50:39 +0700

    Add files via upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,81 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.google.devtools.ksp)
+    alias(libs.plugins.kotlin.parcelize)
+}
+
+android {
+    namespace = "com.dicoding.picodiploma.loginwithanimation"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.dicoding.picodiploma.loginwithanimation"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+        buildConfigField("String", "ENDPOINT", "\"https://story-api.dicoding.dev/v1/\"")
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        named("release") {
+            isMinifyEnabled = false
+            setProguardFiles(listOf(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"))
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+        buildConfig= true
+    }
+    lint {
+        checkAllWarnings = true
+        warningsAsErrors = false
+        abortOnError = true
+    }
+}
+
+dependencies {
+    //default
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.legacy.support.v4)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    //datastore
+    implementation(libs.datastore.preferences)
+
+    //lifecycle
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+
+    //glide
+    implementation(libs.glide)
+
+    //retrofit api
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.logging.interceptor)
+
+    //Ucrop
+    implementation (libs.yalantis.ucrop)
+
+    implementation (libs.androidx.cardview)
+    implementation ("androidx.cardview:cardview:1.0.0")
+
+}
