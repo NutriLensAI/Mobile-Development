a39cc279b98a967118a7b9ab7ef89e7bec859d48
lordsrule <ardyf1234@gmail.com>
2024-06-01 16:19:04 +0700

    Login to SignUp Adjustment
    Login to SignUp Adjustment

--- app/src/main/java/com/capstone/mobiledevelopment/nutrilens/view/settings/personal/PersonalViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.capstone.mobiledevelopment.nutrilens.view.settings.personal
-
-import androidx.lifecycle.ViewModel
-
-class PersonalViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
