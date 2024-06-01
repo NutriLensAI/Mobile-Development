79282590a401a57f6d42beec099e182ea04267fb
lordsrule <ardyf1234@gmail.com>
2024-06-01 16:05:50 +0700

    backupp needed
    backupp needed

--- app/src/main/java/com/capstone/mobiledevelopment/nutrilens/view/settings/password/PasswordViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.capstone.mobiledevelopment.nutrilens.view.settings.password
-
-import androidx.lifecycle.ViewModel
-
-class PasswordViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
