f78565a13075f6cfb553b98a9e93d53d67523ecb
Pavelmez <160759384+Pavelmez@users.noreply.github.com>
2024-06-01 19:06:26 +0700

    update still working it

--- app/src/main/java/com/capstone/mobiledevelopment/nutrilens/view/settings/personal/PersonalFragment.kt
+++ app/src/main/java/com/capstone/mobiledevelopment/nutrilens/view/settings/personal/PersonalFragment.kt
@@ -1,12 +1,15 @@
 package com.capstone.mobiledevelopment.nutrilens.view.settings.personal
 
+import android.content.Intent
 import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.capstone.mobiledevelopment.nutrilens.R
+import com.capstone.mobiledevelopment.nutrilens.view.settings.personal.changepersonal.PersonalActivity
 
 class PersonalFragment : Fragment() {
 
@@ -24,6 +27,15 @@ class PersonalFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.fragment_personal, container, false)
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_personal, container, false)
+
+        val btnChangePersonalData = view.findViewById<Button>(R.id.btnChangePersonalData)
+        btnChangePersonalData.setOnClickListener {
+            val intent = Intent(activity, PersonalActivity::class.java)
+            startActivity(intent)
+        }
+
+        return view
     }
 }
--- app/src/main/java/com/capstone/mobiledevelopment/nutrilens/view/settings/personal/changepersonal/PersonalActivity.kt
+++ app/src/main/java/com/capstone/mobiledevelopment/nutrilens/view/settings/personal/changepersonal/PersonalActivity.kt
@@ -6,16 +6,25 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.capstone.mobiledevelopment.nutrilens.R
+import com.capstone.mobiledevelopment.nutrilens.view.settings.personal.changepersonal.activitylevel.ActivityLevelFragment
 
 class PersonalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_personal)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Check if this is the first creation
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, ActivityLevelFragment.newInstance())
+                .commit()
         }
     }
 }
