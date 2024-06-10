0355eae2f8299233a75dd82665c7fcc957da2329
Pavelmez <160759384+Pavelmez@users.noreply.github.com>
2024-06-10 13:52:55 +0700

    ready sleep

--- app/src/main/java/com/capstone/mobiledevelopment/nutrilens/data/database/sleep/SleepDatabase.kt
+++ app/src/main/java/com/capstone/mobiledevelopment/nutrilens/data/database/sleep/SleepDatabase.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
-@Database(entities = [SleepData::class], version = 1)
+@Database(entities = [SleepData::class], version = 2)
 abstract class SleepDatabase : RoomDatabase() {
     abstract fun sleepDataDao(): SleepDataDao
 
@@ -13,13 +15,22 @@ abstract class SleepDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: SleepDatabase? = null
 
+        val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                // Perform the necessary SQL commands to migrate the schema
+                database.execSQL("ALTER TABLE sleep_data ADD COLUMN sleepCount INTEGER NOT NULL DEFAULT 0")
+            }
+        }
+
         fun getDatabase(context: Context): SleepDatabase {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     SleepDatabase::class.java,
                     "sleep_database"
-                ).build()
+                )
+                    .addMigrations(MIGRATION_1_2)
+                    .build()
                 INSTANCE = instance
                 instance
             }
