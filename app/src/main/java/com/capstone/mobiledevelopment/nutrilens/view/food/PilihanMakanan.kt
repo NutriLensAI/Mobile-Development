5f1233c1d7724ddbd3df8ff9c9599d520717dbfc
lordsrule <ardyf1234@gmail.com>
2024-06-12 10:22:04 +0700

    Adding Search Function
    Adding Search Function

--- app/src/main/java/com/capstone/mobiledevelopment/nutrilens/view/food/PilihanMakanan.kt
+++ app/src/main/java/com/capstone/mobiledevelopment/nutrilens/view/food/PilihanMakanan.kt
@@ -1,26 +1,26 @@
 package com.capstone.mobiledevelopment.nutrilens.view.food
 
-import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.room.Room
 import com.capstone.mobiledevelopment.nutrilens.R
-import com.capstone.mobiledevelopment.nutrilens.view.adapter.food.Food
 import com.capstone.mobiledevelopment.nutrilens.view.adapter.food.FoodAdapter2
+import com.capstone.mobiledevelopment.nutrilens.view.adapter.food.FoodResponse
 import com.capstone.mobiledevelopment.nutrilens.view.adapter.food.RetrofitInstance
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import java.io.IOException
 
 class PilihanMakanan : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var foodAdapter2: FoodAdapter2
+    private lateinit var allFoodList: List<FoodResponse>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +33,15 @@ class PilihanMakanan : AppCompatActivity() {
         recyclerView.adapter = foodAdapter2
 
         fetchFoodData()
+
+        val searchBar: EditText = findViewById(R.id.search_bar)
+        searchBar.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                filter(s.toString())
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
     }
 
     private fun fetchFoodData() {
@@ -40,12 +49,20 @@ class PilihanMakanan : AppCompatActivity() {
             try {
                 val response = RetrofitInstance.api.getFoodData()
                 withContext(Dispatchers.Main) {
-                    foodAdapter2.updateList(response)
+                    allFoodList = response
+                    foodAdapter2.updateList(allFoodList)
                 }
             } catch (e: IOException) {
                 e.printStackTrace()
                 // Handle error here, maybe show a Toast message
             }
         }
+    }
+
+    private fun filter(text: String) {
+        val filteredList = allFoodList.filter {
+            it.name.contains(text, ignoreCase = true)
+        }
+        foodAdapter2.updateList(filteredList)
     }
 }
