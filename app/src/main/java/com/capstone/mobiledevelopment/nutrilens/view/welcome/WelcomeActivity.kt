0fd714e921fed41b773661c513eadd60f29b2593
lordsrule <ardyf1234@gmail.com>
2024-06-18 22:08:14 +0700

    Rollback
    Rollback

--- app/src/main/java/com/capstone/mobiledevelopment/nutrilens/view/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/capstone/mobiledevelopment/nutrilens/view/welcome/WelcomeActivity.kt
@@ -7,6 +7,8 @@ import android.view.animation.AnimationSet
 import android.view.animation.AnimationUtils
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.WindowCompat
 import androidx.lifecycle.lifecycleScope
 import com.capstone.mobiledevelopment.nutrilens.R
 import com.capstone.mobiledevelopment.nutrilens.data.pref.UserModel
@@ -73,16 +75,15 @@ class WelcomeActivity : AppCompatActivity() {
     }
 
     private fun setupView() {
-        @Suppress("DEPRECATION")
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.insetsController?.hide(WindowInsets.Type.statusBars())
-        } else {
-            window.setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-            )
+        WindowCompat.setDecorFitsSystemWindows(window, true)
+        WindowCompat.getInsetsController(window, window.decorView).let { controller ->
+            controller.isAppearanceLightStatusBars =
+                true // Optional: Set status bar content to dark
         }
         supportActionBar?.hide()
+
+        // Set status bar color to green
+        window.statusBarColor = ContextCompat.getColor(this, R.color.white)
     }
 
     private fun setupAction() {
@@ -103,7 +104,13 @@ class WelcomeActivity : AppCompatActivity() {
     private fun loginAsGuest() {
         val userPreference = UserPreference.getInstance(dataStore)
         lifecycleScope.launch {
-            val guestUser = UserModel(email = "guest@guest.com", token = "guest_token", isLogin = true, username = "Guest", isGuest = true)
+            val guestUser = UserModel(
+                email = "guest@guest.com",
+                token = "guest_token",
+                isLogin = true,
+                username = "Guest",
+                isGuest = true
+            )
             userPreference.saveSession(guestUser)
             val intent = Intent(this@WelcomeActivity, MainActivity::class.java)
             startActivity(intent)
